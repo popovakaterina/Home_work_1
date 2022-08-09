6dc3d26aabfc03cde614d546f0fd10625eafd23a
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-09 14:07:09 +0500

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+if ( number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
